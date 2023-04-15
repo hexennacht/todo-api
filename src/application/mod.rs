5f2d7ad8df57da64b705e62791210440b26a4739
Taufik R Firdaus <taufik@ruangguru.com>
2023-04-15 11:33:38 +0700

    BOILER PLATE

---
+++ src/application/mod.rs
@@ -0,0 +1,4 @@
+pub mod dto;
+pub mod handler;
+pub mod request;
+pub mod response;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+pub mod application;
+pub mod domain;
+pub mod infrastructure;
+pub mod route;
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+
+    app.at("/").get(|_| async { Ok("Hello, World!") });
+
+    app.listen("0.0.0.0:8000").await?;
+
+    Ok(())
 }
